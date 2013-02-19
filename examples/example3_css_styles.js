e65c6a8a5cdeb4df7e8d9cbbf25f844d5d5885cb
Brent Noorda <brent.noorda@gmail.com>
2013-02-19 13:53:30 -0800

    show how to use cssCamelCase

---
+++ examples/example3_css_styles.js
@@ -0,0 +1,25 @@
+/*jslint white:false plusplus:false browser:true nomen:false */
+/*globals $, funcyTag */
+
+var div=funcyTag('div');
+
+function build_example_html()
+{
+    var tag;
+    tag = div( { id:'outer', cssFontSize:'20px', cssColor:'red' },
+            'begin outer div',
+            div( { id:'middle', cssFontSize:'16px', cssColor:'green', cssMarginLeft:'12px' },
+              'begin middle div',
+              div( { id:'middle', cssFontSize:'12px', cssColor:'blue', cssMarginLeft:'12px' },
+                'center div'
+              ),
+              'end middle div'
+            ),
+            'end outer div'
+          );
+    return String(tag);
+}
+
+$(document).ready(function() {
+    $('#container').html( build_example_html() );
+});
