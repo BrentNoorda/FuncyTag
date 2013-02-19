19eec18eddd655050a0505bf68ee8e81ec25745b
Brent Noorda <brent.noorda@gmail.com>
2013-02-19 15:27:16 -0800

    add lots of examples Merge commit 'd0e86ecad709a77bada91e53a86ed7614df6fd33'

---
+++ examples/example4_css_units.js
@@ -0,0 +1,25 @@
+/*jslint white:false plusplus:false browser:true nomen:false */
+/*globals $, funcyTag */
+
+var div=funcyTag('div');
+
+function build_example_html()
+{
+    var t;
+    t = div( { id:'outer', cssFontSize_px:20, cssColor:'red' },
+          'begin outer div',
+          div( { id:'middle', cssFontSize_px:16, cssColor:'green', cssMarginLeft_em:2 },
+            'begin middle div',
+            div( { id:'middle', cssFontSize_px:12, cssColor:'blue', cssMarginLeft_em:2 },
+              'center div'
+            ),
+            'end middle div'
+          ),
+          'end outer div'
+        );
+    return String(t);
+}
+
+$(document).ready(function() {
+    $('#container').html( build_example_html() );
+});
