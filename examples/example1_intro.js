221b50e2d99fa9ce2038ceb97f965537844c0a44
Brent Noorda <brent.noorda@gmail.com>
2013-02-19 13:33:18 -0800

    better examples

--- examples/example1_intro.js
+++ examples/example1_intro.js
@@ -1,29 +1,17 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
 /*globals $, funcyTag */
 
+var p=funcyTag('P');
+
 function build_example_html()
 {
-    var tag, p=funcyTag('P'), br=funcyTag('BR'), input=funcyTag('input');
-    tag = p( { id:'outty', cssFontSize:'200%', cssColor:'red' },
-            'The test worked.',
-             p( { id:'inny', cssBackroundColor:'yellow', cssFontSize:'80%' },
-                'some',
-                input( { type:'input', value:'def&amp;au&quot;lt value'} ),
-                input( { type:'input', _value:'def&amp;au&quot;lt value'} ),
-                // maybe the underscore means to escape it
-                'more',
-                br(),
-                'text'
-             )
+    var tag;
+    tag = p( { id:'outty', style:'font-size:200%;color:red;' },
+             "I'm a little teacup, short and stout."
           );
     return String(tag);
 }
 
-function main()
-{
-    $('#container').html( build_example_html() );
-}
-
 $(document).ready(function() {
-    main();
+    $('#container').html( build_example_html() );
 });
