073abbc19c996ee25cf2046e525b41abc6027c08
Brent Noorda <brent.noorda@gmail.com>
2013-02-20 08:17:15 -0800

    show off arrays

--- examples/example6_array_contents.js
+++ examples/example6_array_contents.js
@@ -3,15 +3,27 @@
 
 var div=funcyTag('div'), p=funcyTag('P');
 
+function count_to_ten()
+{
+    var ret = [];
+    for ( i = 1; i <= 10; i++ )
+    {
+        ret.push(i);
+    }
+    return ret;
+}
+
 function build_example_html()
 {
     var t;
-    t = div( {},
+    t = div({},
           p({},'regular paragraph 1'),
           [ p({},'Paragraph 1 in array'), undefined, p({},'Paragraph 2 in array'), p({},'Paragraph 3 in array') ],
           p({},'regular paragraph 2'),
           [ ],  // empty array results in nothing
-          p({},'regular paragraph 3')
+          p({},'regular paragraph 3'),
+          count_to_ten(),
+          p({},'regular paragraph 4')
         );
     return String(t);
 }
