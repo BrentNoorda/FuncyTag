210461d13d6331a6d7ad8df43526f9cf4593c78b
Brent Noorda <brent.noorda@gmail.com>
2013-02-21 17:05:57 -0800

    more good examples

--- examples/example8_behavior_modification.js
+++ examples/example8_behavior_modification.js
@@ -1,11 +1,11 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
 /*globals $, funcyTag */
 
-var div=funcyTag('div'), ul=funcyTag('ul'), li=funcyTag('li'), span=funcyTag('span');
+var div=funcyTag('div'), ul=funcyTag('ul'), li=funcyTag('li',{_nobrin:true}), span=funcyTag('span');
 
 // create a good_li tag that is a green <li>, and bad_li tag that is red
-var good_li = funcyTag( 'li', {cssColor:'green'} ),
-    bad_li = funcyTag( 'li', {cssColor:'red'} );
+var good_li = funcyTag( 'li', {_nobrin:true,cssColor:'green'} ),
+    bad_li = funcyTag( 'li', {_nobrin:true,cssColor:'red'} );
 
 var green = funcyTag('span',{cssColor:'green',_nobrout:true,_nobrin:true});
 var red = funcyTag('span',{cssColor:'red',_nobrout:true,_nobrin:true});
@@ -14,23 +14,25 @@ var big = funcyTag('span',{cssFontSize_pct:200,_nobrout:true,_nobrin:true});
 function xxxbuild_example_html()
 {
     var t, d1 = funcyTag('div'), d2 = funcyTag('div'), d3 = funcyTag('div');
-    t = d1({id:1},d2({id:2},d3({id:3},'blah')));
+    //t = d1({id:1},d2({id:2},d3({id:3},'blah')));
+    //t = d1({},d2({},'dog','cat'));
+    t = d1(d2({}),String('kity'),'dog','cat')
     return String(t);
 }
 
 function build_example_html()
 {
     var t;
-    t = div({},
-          'foods: ' + green({},big({},'L'),'ike') + ', ',
-          red({},'hate'), ',', 'or indifferent',
-          ul({},
-            li({},'rice'),
-            bad_li({},'okra'),
-            good_li({},'pizza'),
-            good_li({},'ice cream'),
-            bad_li({},'dog poop'),
-            li({},'chicken')
+    t = div(
+          'foods: ' + green(big('L'),'ike') + ', ',
+          red(big('H'),'ate'), ',', 'or ',big('I'),'ndifferent',
+          ul(
+            li('rice'),
+            bad_li('okra'),
+            good_li('pizza'),
+            good_li('ice cream'),
+            bad_li({cssFontStyle:'italic'},'dog poop'),
+            li('chicken')
           )
         );
     return String(t);
