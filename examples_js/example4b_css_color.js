e57f08ebed3fd03f5970bf9ab7c1d6ada65c82af
Brent Noorda <brent.noorda@gmail.com>
2013-06-18 00:24:43 -0700

    simple color functions

--- examples_js/example4b_css_color.js
+++ examples_js/example4b_css_color.js
@@ -8,9 +8,6 @@ function build_example_html()
 {
     var t;
 
-    t = funcyTag.color(0x332211);
-    t.mix(1,1);
-
     t = div(
             p( { cssColor: color(0xFF0000) }, 'red' ),
             p( { cssColor: color('#0F0') }, 'green' ),
@@ -19,7 +16,8 @@ function build_example_html()
             p( { cssColor: color(['#00ff00',0.5]) }, 'opaque green' ),
             p( { cssColor: color([0x0000FF,0.5]) }, 'opaque blue' ),
             p( { cssColor: color(0xff0000).add(color(0x00ff00)) }, 'red+green=yellow' ),
-            p( { cssColor: color(0xff00).sub(color(0x888888)) }, 'green-gray=light green' )
+            p( { cssColor: color(0xff00).sub(color(0x888888)) }, 'green-gray=light green' ),
+            p( { cssColor: color(0xff0000).mix(color(0x00ff00),0.75) }, '75%red,25%green' )
         );
     return String(t);
 }
